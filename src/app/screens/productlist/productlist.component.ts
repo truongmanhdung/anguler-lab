7b99e7fb0292d62efb781ee88305ff186a3c7623
truongmanhdung <dungtmph12934@fpt.edu.vn>
2022-01-17 08:00:44 +0700

    update

--- src/app/screens/productlist/productlist.component.ts
+++ src/app/screens/productlist/productlist.component.ts
@@ -38,8 +38,7 @@ export class ProductlistComponent implements OnInit {
     {
       productId: 8,
       productName: 'Saw',
-      productCode: 'TBX-0022',
-      releaseDate: 'May 15, 2016',
+      productCode: 'TBX-0022'      releaseDate: 'May 15, 2016',
       description: '15-inch steel blade hand saw',
       price: 11.55,
       starRating: 3.7,
@@ -62,6 +61,11 @@ export class ProductlistComponent implements OnInit {
   ];
   loading = false;
   searchText = '';
+  filter(e: any){
+    this.searchText = e.target.value;
+    let productFilter = this.productList.filter((product: IProduct) => product.productName.toLowerCase().includes(e.target.value.toLowerCase()));
+    this.products = productFilter;
+  }
   hideImage(image: String) {
     let productHide = this.productList.find((product: IProduct) => product.imageUrl === image);
     if(productHide){
